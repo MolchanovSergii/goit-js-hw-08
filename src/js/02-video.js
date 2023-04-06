f69c9b6277240c9145b8933cd02f898798d5be77
MolchanovSergii <molchanse@gmail.com>
2023-04-06 22:58:51 +0300

    add task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', function (data) {
+    console.log(data.seconds);
+    const currentTime = data.seconds;
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(currentTime));
+});
+
+
+const stopTime = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(stopTime).then(function (){});
